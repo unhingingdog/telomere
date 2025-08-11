d7d43ad6626ba2596e6e43530a4d582f09abe67b
Hamish Gilkison <hamishgilkison@gmail.com>
2025-08-11 20:24:00 +1200

    remove unused lib.ts

--- src/lib.ts
+++
@@ -1,8 +0,0 @@
-import { initTelomere } from "./telomere/telomere-wrapper";
-import type { ParseResult } from "./telomere/wasm-interface-type";
-
-const { processDelta } = await initTelomere();
-
-console.log(processDelta("{"));
-
-export { initTelomere, type ParseResult };
